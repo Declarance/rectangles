cf5f0925b26b4e9497cd5fff2629421fa2cc7e1a
Declarance <declarance@gmail.com>
2018-06-04 17:05:00 +0500

    Print Rect

--- src/rectangles/Rectangle.java
+++ src/rectangles/Rectangle.java
@@ -1,4 +1,22 @@
 package rectangles;
 
-public class Rectangle {
+public class Rectangle{
+    public int x1,y1,x2,y2;
+
+    public Rectangle(int x1,int y1,int x2,int y2){
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public Rectangle(int width, int height) {
+        this(0, 0, width, height);
+    }
+
+    public Rectangle (){ this(0,0,0,0); }
+
+    public print (){
+        System.out.println("Rectangle: "+this.x1+" "+this.y1+" "+this.x2+" "+this.y2)
+    }
 }
